9f19cfa0b04f5bd0441b57823bf47f1f8a227664
yingspvd <32532147+yingspvd@users.noreply.github.com>
2018-04-05 17:45:24 +0700

    new score

--- appying/static/js/score.js
+++ appying/static/js/score.js
@@ -2,22 +2,41 @@ var db = firebase.database();
 var btn = document.getElementById('btn');
 
 var name = document.getElementById('player').value;
+var whoWin; //ชื่อคนชนะ
+var scoreWin; //คะแนนสูงสุด
 var score ;
 
 var ref = firebase.database().ref().child('player/'+name+'/score');
 
+//ShowScore
+var scoreBoard = firebase.database().ref('player').once('value').then(function(snapshot) {
+var player = snapshot.val()
+    data = []
 
+    Object.keys(player).map(a => {
+        data.push({
+            name: a,
+            score: player[a].score
+        })
+    })
 
-//ref.on("player/"+name+"/score".value, function(snapshot) 
-ref.on('value', function(snapshot) 
+    data = data.sort((a,b) => a.score - b.score).reverse()
+
+    whoWin=data[0].name
+    scoreWin=data[0].score
+
+    console.log(data[0].name)
+    console.log(data[0].score)
+
+    document.getElementById('Username').innerHTML = whoWin
+    document.getElementById('Score').innerHTML = scoreWin
+})
+
+function goPlayAgain()
 {
-    console.log(snapshot.val());
-    score=snapshot.val();
-    document.getElementById('Score').innerHTML = score
-});
+    window.location.href = "http://localhost:8000/" ;
+}
 
 
-document.getElementById('Username').innerHTML = name
 
-console.log(name)
-console.log(score)
+
